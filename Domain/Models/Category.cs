e1ac6578abbceafb383365d38b00ee4d0cc890d3
IsakRegell <Isak.regell@gmail.com>
2025-05-26 21:35:59 +0200

    Lägger till min Category Modeld. Gjorde en liten ändring i User. Felstavning samt lite fil med {}

---
+++ Domain/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }         // Primärnyckel
+        public Guid UserId { get; set; }             // Foreign key till användare
+        public string Name { get; set; } = string.Empty;
+
+        //Navigation property – länkar till användaren som äger kategorin
+        public User? User { get; set; }
+    }
+}
--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -1,19 +1,23 @@
-﻿namespace Domain.Moduls;
-
-//Här definierar vi klassen User
-// vi ska lägga till userid, user name, email, passwordhash, currency, 
-public class User
+﻿namespace Domain.Models
 {
-    public Guid UserId { get; set; } 
-    public string UserName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public string Currency { get; set; } = string.Empty;
-    public Guid DefaultCategoryId { get; set; }
-    
-    //Navigation property
-    
-   // public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
-   // public ICollection<Category> Categories { get; set; } = new List<Category>();
+    //Här definierar vi klassen User
+    // vi ska lägga till userid, user name, email, passwordhash, currency, 
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Currency { get; set; } = string.Empty;
+        public Guid DefaultCategoryId { get; set; }
+
+        //Navigation property
+
+        // public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+        // public ICollection<Category> Categories { get; set; } = new List<Category>();
+    }
 }
+
+
+
 
